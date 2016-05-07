d01f9ca19cb04bf745c70d7732b436c7206c4377
Tenzin <sinzin91@gmail.com>
2016-05-07 13:23:59 -0700

    step 13: express api routes

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ require('babel-register');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var async = require('async');
+var request = require('request');
+var xml2js = require('xml2js');
 
 var express = require('express');
 var swig  = require('swig');
@@ -28,6 +31,76 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * POST /api/characters
+ * Adds new character to the database.
+ */
+app.post('/api/characters', function(req, res, next) {
+  var gender = req.body.gender;
+  var characterName = req.body.name;
+  var characterIdLookupUrl = 'https://api.eveonline.com/eve/CharacterID.xml.aspx?names=' + characterName;
+
+  var parser = new xml2js.Parser();
+
+  async.waterfall([
+    function(callback) {
+      request.get(characterIdLookupUrl, function(err, request, xml) {
+        if (err) return next(err);
+        parser.parseString(xml, function(err, parsedXml) {
+          if (err) return next(err);
+          try {
+            var characterId = parsedXml.eveapi.result[0].rowset[0].row[0].$.characterID;
+
+            Character.findOne({ characterId: characterId }, function(err, character) {
+              if (err) return next(err);
+
+              if (character) {
+                return res.status(409).send({ message: character.name + ' is already in the database.' });
+              }
+
+              callback(err, characterId);
+            });
+          } catch (e) {
+            return res.status(400).send({ message: 'XML Parse Error' });
+          }
+        });
+      });
+    },
+    function(characterId) {
+      var characterInfoUrl = 'https://api.eveonline.com/eve/CharacterInfo.xml.aspx?characterID=' + characterId;
+
+      request.get({ url: characterInfoUrl }, function(err, request, xml) {
+        if (err) return next(err);
+        parser.parseString(xml, function(err, parsedXml) {
+          if (err) return res.send(err);
+          try {
+            var name = parsedXml.eveapi.result[0].characterName[0];
+            var race = parsedXml.eveapi.result[0].race[0];
+            var bloodline = parsedXml.eveapi.result[0].bloodline[0];
+
+            var character = new Character({
+              characterId: characterId,
+              name: name,
+              race: race,
+              bloodline: bloodline,
+              gender: gender,
+              random: [Math.random(), 0]
+            });
+
+            character.save(function(err) {
+              if (err) return next(err);
+              res.send({ message: characterName + ' has been added successfully!' });
+            });
+          } catch (e) {
+            res.status(404).send({ message: characterName + ' is not a registered citizen of New Eden.' });
+          }
+        });
+      });
+    }
+  ]);
+});
+
 
 app.use(function(req, res) {
   Router.match({ routes: routes.default, location: req.url }, function(err, redirectLocation, renderProps) {
@@ -66,6 +139,7 @@ io.sockets.on('connection', function(socket) {
 server.listen(app.get('port'), function() {
 	console.log('express server listening on port ' + app.get('port'));
 });
+
 
 
 
